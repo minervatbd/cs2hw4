627f257c1eea7e745f7a76734d4ad409c7b51bf5
Minerva <minervatbd>
2024-11-05 15:59:01 -0500

    class and blank methods

--- HopStepGame.java
+++ HopStepGame.java
@@ -3,3 +3,24 @@
  * COP3503 Fall 2024
  * Programming Assignment 4
  */
+
+ public class HopStepGame 
+ {
+    // TODO
+    public int minCost(int squares[], int len)
+    {
+        return 0;
+    }
+
+    // TODO
+    public int minCostMemoization(int squares[], int len)
+    {
+        return 0;
+    }
+
+    // TODO
+    public int minCostTabulation(int squares[], int len)
+    {
+        return 0;
+    }
+ }
