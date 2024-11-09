8e7c05c3c6077ac879af25cad75d6e01e086856a
Minerva <minervatbd>
2024-11-09 14:24:29 -0500

    tabulate almsot works

--- HopStepGame.java
+++ HopStepGame.java
@@ -33,10 +33,27 @@
         return memo[len] = squares[len] + min(minCostMemoization(squares, len - 1, memo), minCostMemoization(squares, len - 2, memo));
     }
 
-    // TODO
+    // tabulated approach
     public int minCostTabulation(int squares[])
     {
-        return 0;
+        int n = squares.length;
+
+        // table
+        int[] tab = new int[n];
+
+        // base cases
+        tab[0] = squares[0];
+        tab[1] = squares[1];
+
+        int c;
+
+        // bottom up approach
+        for (c = 2; c < n - 1; c++)
+        {
+            tab[c] = squares[c] + min(tab[c - 1], tab[c - 2]);
+        }
+
+        return tab[c - 1];
     }
 
     // helper function, returns smaller of two ints
